b82799c32944e1374fd1d912cf5b6f2fc1e4a163
MuenYu <parasomnia97@gmail.com>
2024-03-18 18:25:42 +1000

    refine logic of anki-parser

--- anki-pot-parser/main.py
+++ anki-pot-parser/main.py
@@ -1,27 +1,32 @@
-import csv
 import re
 from stardict import DictCsv
+import csv
 
 source = '.\\pot.txt'
-output = '.\\pot.csv'
+output = '.\\result.csv'
+err = '.\\error.txt'
 ecdict = DictCsv(filename='ecdict.csv')
 regex = r'\[(.*?)\]'
 
 raw_data = []
-with open(source) as f:
+with open(source, 'r') as f, open(output, 'w') as d, open(err, 'w') as e:
+    writer = csv.writer(d)
     for line in f:
         sections = line.split('\t')
         match = re.search(regex, sections[0])
+        # if block bracket exist
         if match:
             word = match.group(1)
-            val = ecdict.query(word)
-            if val:
-                definition = val.get('definition')
-                translation = val.get('translation')
-                raw_data.append([word, sections[1], definition, translation, sections[0]])
-            else:
-                print(word)
-
-with open(output, 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(raw_data)
+            example = sections[0] or ''
+            cn_example = sections[1] or ''
+        else:
+            word = sections[0]
+            example = ''
+            cn_example = ''
+        val = ecdict.query(word)
+        if val:
+            definition = val.get('definition')
+            translation = val.get('translation')
+            writer.writerow([word, definition, translation, example, cn_example])
+        else:
+            e.write(f'{line}')
